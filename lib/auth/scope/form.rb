1a25476b474807e436f43e12c896fdd66c3db853
isubas <irfan.subas@uzem.omu.edu.tr>
2019-03-21 08:16:04 +0300

    [WIP] Filterelerde sorgulama türü seçilebilsin

--- lib/auth/scope/form.rb
+++ lib/auth/scope/form.rb
@@ -15,17 +15,20 @@ module Auth
 
       def fields
         filters.map do |attribute, option|
-          generate_form_field(attribute, option)
+          {
+            value_field: generate_field_for_value(attribute, option),
+            query_type_field: generate_field_for_query_type(attribute)
+          }
         end
       end
 
       private
 
-      def generate_form_field(attribute, option)
+      def generate_field_for_value(attribute, option)
         collection = collection_for(option)
 
         Field.new(
-          name: attribute_name_prepare(attribute),
+          name: attribute.to_s + StoreAccessor::SUFFIX_FOR_VALUE_ACCESSOR,
           as: collection.present? ? :select : option.field_type,
           collection: collection,
           required: option.presence,
@@ -34,15 +37,20 @@ module Auth
         )
       end
 
+      def generate_field_for_query_type(attribute)
+        Field.new(
+          name: attribute.to_s + StoreAccessor::SUFFIX_FOR_QUERY_TYPE_ACCESSOR,
+          as: :select,
+          collection: Utils::Arel::PREDICATES,
+          required: true
+        )
+      end
+
       def collection_for(option)
         case option.collection
         when Proc then option.collection.call
         when Array then option.collection
         end
-      end
-
-      def attribute_name_prepare(attribute)
-        attribute.to_s + StoreAccessor::SUFFIX_FOR_STORE_ACCESSOR
       end
 
       def hint(option)
--- lib/auth/scope/store_accessor.rb
+++ lib/auth/scope/store_accessor.rb
@@ -4,31 +4,40 @@ module Auth
   module Scope
     module StoreAccessor
       extend ActiveSupport::Concern
-      SUFFIX_FOR_STORE_ACCESSOR = '_value'
+      SUFFIX_FOR_VALUE_ACCESSOR = '_value'
+      SUFFIX_FOR_QUERY_TYPE_ACCESSOR = '_query_type'
 
       included do
-        after_initialize :set_store_accessor
+        after_initialize :set_store_accessor_for_values
 
-        def auth_store_accessors
-          @auth_store_accessors ||= _scope_klass.filter_attributes.map do |key|
-            key.to_s + SUFFIX_FOR_STORE_ACCESSOR
+        def auth_store_accessors_for_values
+          @auth_store_accessors_for_values ||= begin
+            scope_klass.filter_attributes.map do |key|
+              value_accessor_for(key)
+            end
           end
         end
 
         def permited_attributes_for_values
           @permited_attributes_for_values ||= begin
-            _scope_klass.filters.each_with_object([]) do |(key, option), attributes|
-              key = _store_accessor_key_for(key)
+            scope_klass.filters.map do |key, option|
+              key = value_accessor_for(key)
               attributes << (option.multiple ? { key => [] } : key)
             end
           end
         end
 
+        def permited_attributes_for_query_methods
+          @permited_attributes_for_query_methods ||= begin
+            scope_klass.filter_attributes.map { |attribute| query_method_accessor_for(attribute) }
+          end
+        end
+
         private
 
-        def set_store_accessor
-          _scope_klass.filter_attributes.each do |key|
-            method = _store_accessor_key_for(key)
+        def set_store_accessor_for_values
+          scope_klass.filter_attributes.each do |key|
+            method = value_accessor_for(key)
 
             define_singleton_method(method) { values[key] }
             define_singleton_method("#{method}=") do |value|
@@ -37,12 +46,16 @@ module Auth
           end
         end
 
-        def _store_accessor_key_for(key)
-          key.to_s + SUFFIX_FOR_STORE_ACCESSOR
+        def value_accessor_for(key)
+          key.to_s + SUFFIX_FOR_VALUE_ACCESSOR
         end
 
-        def _scope_klass
-          name.constantize
+        def query_method_accessor_for(key)
+          key.to_s + SUFFIX_FOR_VALUE_ACCESSOR
+        end
+
+        def scope_klass
+          @_scope_klass ||= name.constantize
         end
       end
     end
